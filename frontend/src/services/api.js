90e4e67def9fbbd957b52205674c5dc3c514ad86
Jacques Gentil <thierrynamahirwe@gmail.com>
2026-07-05 16:38:26 +0200

    can

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://jackweb.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
